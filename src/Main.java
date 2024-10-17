ec3f1bfd074508ef1d2b9ed51a21f404190b42e7
s99080 <s99080@pollub.edu.pl>
2024-10-17 09:14:07 +0200

    feat\main page class with functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        String username = "";
         System.out.println("Hello world!");
+
+        Main_page main_page = new Main_page(username);
+        main_page.showPage();
     }
 }
---
+++ src/Main_page.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main_page {
+
+    private String current_user;
+
+    public Main_page (String u){
+        current_user = u;
+    }
+
+    public void showPage (){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to application main page user " +current_user);
+        System.out.println("Please input any key to logout ...");
+        sc.nextLine();
+    }
+
+    public void setUser (String usr) {
+        current_user = usr;
+    }
+
+    public String getUser () {
+        return current_user;
+    }
+}
