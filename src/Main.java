fcca36d65f93a326e954a78f581adeb33b4e4252
stan10 <s97754@pollub.edu.pl>
2024-10-17 09:22:33 +0200

    feat: added Register class with functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static class Register{
+
+        String username, password;
+
+        public Register(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public boolean confirmPassword(String password) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Wpisz hasło: ");
+            String inputPassword = scanner.nextLine();
+            return password.equals(inputPassword);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Register register = new Register("Cristiano7", "cr7-1985");
+
+        if(register.confirmPassword(register.password)) {
+            System.out.println("Zarejestrowano poprawnie");
+        } else {
+            System.out.println("Nie udało się zarejestrować");
+        }
     }
 }
